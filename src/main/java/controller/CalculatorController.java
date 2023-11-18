b05448e7c2779af66175c6b2e915299bc044d746
DiplineRU <artgul52@gmail.com>
2023-11-18 12:39:26 +0300

    complate hommework 15

---
+++ src/main/java/controller/CalculatorController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import service.CalculatorService;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+        private final CalculatorService calculatorService;
+    public CalculatorController(CalculatorService calculatorService) {
+            this.calculatorService = calculatorService;
+        }
+        @GetMapping
+        public String hello () {
+            return "Добро пожаловать в калькулятор!";
+        }
+        @GetMapping("/plus")
+        public String plus (@RequestParam(value = "num1", required = false) Number a,
+                @RequestParam(value = "num2", required = false) Number b){
+            return calculate(a,b, "+");
+        }
+        @GetMapping("/minus")
+        public String minus (@RequestParam(value = "num1", required = false) Number a,
+                @RequestParam(value = "num2", required = false) Number b){
+            return calculate(a, b, "-");
+        }
+        @GetMapping("/multiply")
+        public String multiply (@RequestParam(value = "num1", required = false) Number a,
+                @RequestParam(value = "num2", required = false) Number b){
+            return calculate(a, b, "*");
+        }
+        @GetMapping("/divide")
+        public String divide (@RequestParam(value = "num1", required = false) Number a,
+                @RequestParam(value = "num2", required = false) Number b){
+            return calculate(a, b, "/");
+        }
+        private String calculate (Number a, Number b, String operation){
+            if (a == null || b == null) {
+                return "Какой-то из параметров не передан!";
+            }
+            if (operation.equals("/") && b.intValue() == 0) {
+                return "На ноль делить нельзя!";
+            }
+            Number result = switch (operation) {
+                case "-" -> calculatorService.minus(a, b);
+                case "*" -> calculatorService.multiply(a, b);
+                case "/" -> calculatorService.divide(a, b);
+                default -> calculatorService.plus(a, b);
+            };
+            return "%s %s %s = %s".formatted(a, operation, b, result);
+        }
+    }
---
+++ src/main/java/service/CalculatorService.java
@@ -0,0 +1,25 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+
+    public Number plus(Number a, Number b) {
+        return a.doubleValue() + b.doubleValue();
+    }
+
+    public Number minus(Number a, Number b) {
+        return a.doubleValue() - b.doubleValue();
+    }
+
+    public Number multiply(Number a, Number b) {
+        return a.doubleValue() * b.doubleValue();
+    }
+
+    public Number divide(Number a, Number b) {
+        return a.doubleValue() / b.doubleValue();
+    }
+
+}
